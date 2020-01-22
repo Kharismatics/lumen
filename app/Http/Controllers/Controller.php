8f2bb0dca3e68d79a50dbb6817fc93ec59f6422a
Kharisma Wahyudin <kharismatics@gmail.com>
2020-01-22 11:13:31 +0700

    migration and seed fake data

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,9 +8,11 @@ class Controller extends BaseController
 {
     public function testconnection()
     {
-        // return 'tes';
-        $query="SELECT * from users ";
-        $result = app('db')->select($query);
-        return $result;
+        // // return 'tes';
+        // $query="SELECT * from users ";
+        // $result = app('db')->select($query);
+        // return $result;
+        // return \App\User::all();
+        return \App\Category::all();
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Str;
+use Illuminate\Support\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +14,66 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
+        $faker = Faker::create();
+        foreach (range(1, 10) as $index) {
+            DB::table('users')->insert([
+                'name' => $faker->name,
+                'email' => $faker->freeEmail,
+                'phone' => $faker->e164PhoneNumber,
+                'address' => $faker->address,
+                'password' => '12345678',
+                'created_at' => Carbon::now(),
+            ]);
+        }
+
+        DB::table('categories')->insert([
+            [
+                'name' => 'bag',
+                'created_at' => Carbon::now(),
+            ],
+            [
+                'name' => 'wallet',
+                'created_at' => Carbon::now(),
+            ],
+        ]);
+        
+        foreach (range(1, 10) as $index) {
+            DB::table('products')->insert(
+                [
+                    'name' => $faker->countryCode, 
+                    'category_id' => $faker->numberBetween($min = 1, $max = 2),
+                    'unique_code' => $faker->shuffle('123456abc'),
+                    'base_price' => $faker->numberBetween($min = 50000, $max =100000),
+                    'price' => 150000,
+                    'description' => 'tes',
+                    'created_at' => Carbon::now(),
+                ]
+            );
+        }
+
+        DB::table('stocks')->insert(
+            [
+                'product_id' => 1,
+                'quantity' => 20,
+                'created_at' => Carbon::now(),
+            ]
+        );
+
+        foreach (range(1, 250) as $index) {
+            DB::table('transactions')->insert(
+                [
+                    'product_id' => $faker->numberBetween($min = 1, $max = 10),
+                    'user_id' => $faker->numberBetween($min = 1, $max = 10),
+                    // 'transaction_date' => "".$faker->numberBetween($min = 2018, $max = 2020)."-".$faker->month($max = 'now')."-01",
+                    'transaction_date' => "".$faker->numberBetween($min = 2018, $max = 2020)."-".$faker->month($max = 'now')."-".$faker->numberBetween($min = 1, $max = 28),
+                    'discount' => 1,
+                    'shipping_cost' => $faker->numberBetween($min = 5000, $max = 30000),
+                    'description' => 1,
+                    'remark' => 1,
+                    'transaction_status' => $faker->numberBetween($min = 1, $max = 3),
+                    'created_at' => Carbon::now(),
+                ]
+            );
+        }
     }
 }
